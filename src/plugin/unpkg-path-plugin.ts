4d6bb6f133877db271c8bce13c19c99d6e37fa9b
HungOm <hung.workspace@gmail.com>
2021-08-01 01:50:58 +0800

    dynamically fetching package and bundling with esbuild

---
+++ src/plugin/unpkg-path-plugin.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import * as esbuild from 'esbuild-wasm';
+ 
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log('onResole', args);
+        if(args.path === 'index.js'){
+          return { path: args.path, namespace: 'a' };
+        
+        }else if(args.path==='tiny-test-pkg'){
+          return {
+            path:'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+            namespace:'a',
+          };
+  
+        }
+      
+       
+      });
+ 
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args);
+ 
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: `
+              const message = require('tiny-test-pkg')
+              console.log(message);
+            `,
+          };
+        } 
+        const {data} = await axios.get(args.path);
+        // console.log(data);
+        return {
+          loader:'jsx',
+          contents:data,
+        }
+      });
+    },
+  };
+};
