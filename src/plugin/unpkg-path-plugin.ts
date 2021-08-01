888d10763bfcea4693958f15bdb634264fa096d4
HungOm <hung.workspace@gmail.com>
2021-08-01 22:03:37 +0800

    breaking up Resolve Logic with Filter

--- src/plugin/unpkg-path-plugin.ts
+++ src/plugin/unpkg-path-plugin.ts
@@ -17,19 +17,21 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if(args.path === 'index.js'){
-          return { path: args.path, namespace: 'a' };
-        
+      build.onResolve({filter:/^(index\.js$)/},()=>{
+        // this one only find index and return so no need to be async 
+        return {path:'index.js',namespace:'a'};
+      })
+
+      build.onResolve({filter:/^\.+\//},(args:any)=>{
+      // this regex will find paths with ./ or ../ 
+        return {
+          namespace:'a',
+          path:new URL(args.path,'https://unpkg.com'+args.resolveDir+'/').href,
         }
 
-        if (args.path.includes('./')||args.path.includes('../')){
-          return {
-            namespace:'a',
-            path:new URL(args.path,'https://unpkg.com'+args.resolveDir+'/').href,
-          }
-        }
+      })
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        // this one find any file that match the regex do other jobs like parsing url at the asyncronously 
         return {
           namespace:'a',
           path:`https://unpkg.com/${args.path}`,
