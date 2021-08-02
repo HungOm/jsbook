ab11f43033bf48868e963a1712401493c9efbbb2
HungOm <hung.workspace@gmail.com>
2021-08-02 20:13:41 +0800

    seperating load filters

--- src/plugin/fetch-plugin.ts
+++ src/plugin/fetch-plugin.ts
@@ -12,35 +12,36 @@ export const fetchPlugin = (inputCode: string)=>{
     return {
         name:'fetch-plugin',
         setup(build: esbuild.PluginBuild){
-             
-      build.onLoad({ filter: /.*/ }, async (args: any) => { 
-        if (args.path === 'index.js') {
+
+      build.onLoad({filter:/(^index.js$)/},()=>{
+      
           return {
             loader: 'jsx',
             contents: inputCode,
           };
-        } 
 
-        //check to see if we have already fetched this file 
-        // and if it is in the cache 
-        const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-        // if it is , return it immediately 
-        if(cacheResult){
-          return cacheResult;
-        }
+      });
+
+      build.onLoad({filter:/.css$/},async(args:any)=>{
+        
+        // //check to see if we have already fetched this file 
+        // // and if it is in the cache 
+        // const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        // // if it is , return it immediately 
+        // if(cacheResult){
+        //   return cacheResult;
+        // }
         const {data,request} = await axios.get(args.path);
         // check if the user input is js or css and add to loader
-        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
         const escaped = data
           .replace(/\n/g,'')
           .replace(/"/g,'\\"')
           .replace(/'/g,"\\'")
         const contents =
-        fileType === 'css'?
           `const style = document.createElement('style');
            style.innerText = '${escaped}';
            document.head.appendChild(style);
-          `: data;
+          `;
         //store response in cache
         const result:esbuild.OnLoadResult =  {
           loader:'jsx',
@@ -50,6 +51,31 @@ export const fetchPlugin = (inputCode: string)=>{
 
         await fileCache.setItem(args.path,result)
         return result;
+
+      });
+             
+      build.onLoad({ filter: /.*/ }, async (args: any) => { 
+        
+        //check to see if we have already fetched this file 
+        // and if it is in the cache 
+        const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        // if it is , return it immediately 
+        if(cacheResult){
+          return cacheResult;
+        }
+        const {data,request} = await axios.get(args.path);
+ 
+        //store response in cache
+        const result:esbuild.OnLoadResult =  {
+          loader:'jsx',
+          contents:data,
+          resolveDir: new URL('./', request.responseURL).pathname
+        }
+
+        await fileCache.setItem(args.path,result)
+        return result;
+       
+
       });
 
         }
