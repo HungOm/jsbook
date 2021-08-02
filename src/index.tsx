526f59788fee5b16ade4dfdc3eca0ca2289e4280
HungOm <hung.workspace@gmail.com>
2021-08-03 04:02:31 +0800

    safely handling untrusted code execution via iframe using postMessage for the same host

--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,80 @@
-import * as esbuild from 'esbuild-wasm'
-import { useState,useEffect,useRef } from 'react';
-import ReactDom from 'react-dom';
-import {unpkgPathPlugin} from './plugin/unpkg-path-plugin';
-import { fetchPlugin } from './plugin/fetch-plugin';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
-const App = ()=>{
-    const ref = useRef<any>()
-    const [input ,setInput]=useState('');
-    const [code,setCode]= useState('');
-    const startService = async()=>{
-        ref.current = await esbuild.startService({
-            worker:true,
-            wasmURL:'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-        })
-        // console.log(service)
-    }   
+const App = () => {
+  const ref = useRef<any>();
+  const iframe = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-    useEffect(()=>{
-        startService();
-    },[])
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
-
-
-    const onClick =async ()=>{
-        // console.log(input);
-        if(!ref.current){
-            return;
-        }
-    //    const result =  await ref.current.transform(input,{
-    //         loader:'jsx',
-    //         target:'es2015'
-    //     });
-    const result = await ref.current.build({
-        entryPoints:['index.js'],
-        bundle:true,
-        write:false,
-        plugins:[unpkgPathPlugin(),fetchPlugin(input)],
-        define:{
-            'process.env.NODE_ENV':'"production"',
-            global:'window',
-            //without global -> browser will not know the variable window
-        }
-    })
-        setCode(result.outputFiles[0].text);
-        eval(result.outputFiles[0].text);
- 
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
     }
-    return <div>
-        <textarea value={input} onChange={e=>setInput(e.target.value)}></textarea>
-        <div>
-            <button onClick={onClick}>Submit</button>
-        </div>
-        <pre>
 
-{code}
+    iframe.current.srcdoc = html;
 
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
 
-</pre>
-    </div>;
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+  };
 
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+    </div>
+  );
 };
 
-ReactDom.render(<App/>,document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
