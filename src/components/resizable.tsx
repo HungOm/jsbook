6df814e23be0b4158df718878ec48023f7a59e36
HungOm <hung.workspace@gmail.com>
2021-08-07 03:32:37 +0800

    resize events, update resize props and improve resize performanace by resizing after 100 milliseconds

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,16 +1,39 @@
 import './resizeable.css';
+import { useEffect,useState } from 'react';
 import { ResizableBox,ResizableBoxProps } from "react-resizable";
+
 interface ResizableProps{
     direction:'horizontal'|'vertical';
 }
 const Resizable:React.FC<ResizableProps> = ({direction,children}) =>{
     let ResizableProps:ResizableBoxProps;
+    const [innerHeight,setInnerHeight] = useState(window.innerHeight);
+    const [innerWidth,setInnerWidth] = useState(window.innerWidth);
+    useEffect(()=>{
+        let timer:any;
+        const listener = ()=>{
+            if(timer){
+                clearTimeout(timer);
+            }
+            timer = setTimeout(()=>{
+            // console.log(window.innerWidth,window.innerHeight)
+            setInnerHeight(window.innerHeight);
+            setInnerWidth(window.innerWidth)
+
+            },100)
+
+        };
+        window.addEventListener('resize',listener)
+
+        return ()=>{window.removeEventListener('resize',listener);}
+    
+    },[])
 
     if (direction==="horizontal"){
         ResizableProps={
             className:'resize-horizontal',
-            minConstraints:[window.innerWidth*0.2,Infinity],
-            maxConstraints:[window.innerWidth * 0.75,Infinity], 
+            minConstraints:[innerWidth*0.2,Infinity],
+            maxConstraints:[innerWidth * 0.75,Infinity], 
             height:Infinity, 
             width:window.innerWidth*0.75, 
             resizeHandles:['e']
@@ -19,7 +42,7 @@ const Resizable:React.FC<ResizableProps> = ({direction,children}) =>{
     }else{
         ResizableProps={
             minConstraints:[Infinity,24],
-            maxConstraints:[Infinity,window.innerHeight*0.9], 
+            maxConstraints:[Infinity,innerHeight*0.9], 
             height:300, 
             width:Infinity, 
             resizeHandles:['s']
