eeaa364b693f0fbd446e3764871cf1894f20161d
HungOm <hung.workspace@gmail.com>
2021-09-25 16:46:41 +0800

    mardown editor and github deploy added

---
+++ src/state/actions/index.ts
@@ -0,0 +1,45 @@
+// import { Action } from "redux";
+import { ActionType } from "../action-types";
+import {CellType} from '../cell';
+interface MoveCellAction {
+    type: ActionType.MOVE_CELL;
+    payload: {
+        id: string;
+        direction: 'up' | 'down';
+
+    }
+
+}
+
+interface DeleteCellAction {
+    type: ActionType.DELETE_CELL;
+    payload: string;
+
+}
+
+interface InsertCellBeforeAction {
+    type: ActionType.INSERT_CELL_BEFORE;
+    payload: {
+        id: string;
+        type: CellType;
+    }
+
+}
+
+interface UpdateCellAction {
+    type: ActionType.UPDATE_CELL;
+    payload: {
+        id: string;
+        content: string;
+    }
+
+}
+
+
+export type Action =
+    | MoveCellAction
+    | DeleteCellAction
+    | InsertCellBeforeAction
+    | UpdateCellAction;
+
+
