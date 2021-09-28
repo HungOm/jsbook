eb80f2804f054f9b64119a18a37e86c6e303ab95
HungOm <hung.workspace@gmail.com>
2021-09-28 11:32:26 +0800

    cell reducer setup

---
+++ src/state/reducers/cellsReducer.ts
@@ -0,0 +1,46 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+import { Cell } from '../cell'
+
+
+interface CellState {
+    loading: boolean;
+    error: string | null;
+    order: string[];
+    data: {
+        [key: string]: Cell
+    }
+
+}
+
+const initialState: CellState = {
+    loading: false,
+    error: null,
+    order: []
+    ,
+    data: {}
+};
+
+
+const reducer = (
+    state: CellState = initialState,
+    action: Action): CellState => {
+    //  return state;
+    switch (action.type) {
+        case ActionType.UPDATE_CELL:
+            return state;
+        case ActionType.DELETE_CELL:
+            return state;
+        case ActionType.MOVE_CELL:
+            return state;
+        case ActionType.INSERT_CELL_BEFORE:
+            return state;
+        default:
+            return state;
+    }
+
+};
+
+
+export default reducer;
