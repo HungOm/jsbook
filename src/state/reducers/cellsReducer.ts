06bb5eb03a196045939769409822a57b3ccbe0f0
HungOm <hung.workspace@gmail.com>
2021-09-28 17:34:13 +0800

    removing a cell & cell swapping

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,8 +1,7 @@
+import produce from 'immer';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
-
 import { Cell } from '../cell'
-
 
 interface CellState {
     loading: boolean;
@@ -11,45 +10,54 @@ interface CellState {
     data: {
         [key: string]: Cell
     }
-
 }
-
 const initialState: CellState = {
     loading: false,
     error: null,
     order: [],
     data: {}
 };
-
-
-const reducer = (
+const reducer = produce((
     state: CellState = initialState,
-    action: Action): CellState => {
+    action: Action) => {
     //  return state;
     switch (action.type) {
         case ActionType.UPDATE_CELL:
             const { id, content } = action.payload
-            return {
-                ...state,
-                data: {
-                    ...state.data, 
-                    [id]: {
-                        ...state.data[id], 
-                        content
-                    }
-                }
-            };
+            state.data[id].content = content; //this is simplified by produced from immer
+            // return;
+            return;
+            // the return below is replace with immer right at the line above
+            // return {
+            //     ...state,
+            //     data: {
+            //         ...state.data, 
+            //         [id]: {
+            //             ...state.data[id], 
+            //             content
+            //         }
+            //     }
+            // };
+            // return;
         case ActionType.DELETE_CELL:
-            return state;
+            delete state.data[action.payload]
+            state.order = state.order.filter((id)=>id!==action.payload)
+            return;
         case ActionType.MOVE_CELL:
-            return state;
+            const {direction} = action.payload;
+            const index =state.order.findIndex((id)=>id===action.payload.id)
+            const targeIndex = direction==='up'?index-1 :index+1
+            if(targeIndex<0||targeIndex>state.order.length-1){
+                return;
+            }
+
+            state.order[index] = state.order[targeIndex]
+            state.order[targeIndex] = action.payload.id;
+            return ;
         case ActionType.INSERT_CELL_BEFORE:
             return state;
         default:
             return state;
     }
-
-};
-
-
+});
 export default reducer;
